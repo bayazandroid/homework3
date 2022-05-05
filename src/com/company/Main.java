e74e69686815737b85c9c6ae147660cbe186671c
bayazandroid <bayaz.orozobekov@gmail.com>
2022-05-05 13:24:21 +0300

    Добрый день,ДЗ-3_3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,8 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
         double[] numbers = {2.3, 5.3, 13.1, -1.7, 61.3, -78.5, -98.3, 10.5, 11.7, 13.9, 54.3, 8.7, 9.3, 120.7, 983.9};
-        System.out.println(numbers[2]);
-        System.out.println(Arrays.toString(numbers));
 
         int indexFirstMinusNumber = 0;
         double sum = 0;
@@ -31,6 +29,18 @@ public class Main {
         System.out.println("позиция первого отрицательн. число "+indexFirstMinusNumber);
         System.out.println("средн. арифм. число " +sum/countOfPositiveNumbers);
 
+        System.out.println("до сортировки "+Arrays.toString(numbers));
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers.length-i-1; j++) {
+                if (numbers[j] > numbers[j+1]) {
+                    double temp = numbers[j];
+                    numbers[j] = numbers[j+1];
+                    numbers[j+1] = temp;
+                }
+            }
+        }
+        System.out.println("После сортировки "+Arrays.toString(numbers));
     }
 }
 
